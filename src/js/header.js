b6a21b694b8996da4c6c458b19d78220feb0bacb
Timka09 <tymofij.i.naumenko@gmail.com>
2025-03-09 01:01:47 +0200

    giga fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,112 +1,42 @@
-
-
 const themeChanger = document.querySelector(".header__theme-changer");
 
 function toggleTheme() {
   document.body.classList.toggle("dark-mode");
 }
 
-themeChanger.addEventListener('click', toggleTheme);
+themeChanger.addEventListener("click", toggleTheme);
 
-// const nun1 = document.getElementById(nunn1) 
-// const nuj2 = document.getElementById(nuj2) 
-// const nul3 = document.getElementById(nul3) 
+const nun1 = document.getElementById("nunn1");
 
-// if (nun1) {
-//     nun1.addEventListener('click', () => {
-//         game.classList.add("none")
-//     });
-// } else {
-//     console.log('Элемент не найден!');
-// }
-
-
-// if (nuj2) {
-//     nuj2.addEventListener('click', () => {
-//         game.classList.add("none")
-//     });
-// } else {
-//     console.log('Элемент не найден!');
-// }
-
-// if (nul3) {
-//     nul3.addEventListener('click', () => {
-//         game.classList.add("none")
-//     });
-// } else {
-//     console.log('Элемент не найден!');
-// }
-
-
-const nun1 = document.getElementById("nunn1") 
-nun1.addEventListener('click', () => {
-        document.getElementById("game").classList.replace('game', 'none' )
-        document.getElementById("dino").classList.replace('dino', 'none' )
-        document.getElementById("socker").classList.replace('socker', 'none' )
-        document.getElementById("acquaintance").classList.replace('team', 'none' )
-        document.getElementById("science").classList.replace('science', 'none' )
+nun1.addEventListener("click", () => {
+  document.getElementById("game").classList.replace("game", "none");
+  document.getElementById("dino").classList.replace("dino", "none");
+  document.getElementById("socker").classList.replace("socker", "none");
+  document.getElementById("acquaintance").classList.replace("team", "none");
+  document.getElementById("science").classList.replace("science", "none");
 });
 
+const nuj2 = document.getElementById("nuj2");
 
-
-const nuj2 = document.getElementById("nuj2") 
-
-nuj2.addEventListener('click', () => {
-        document.getElementById("dino").classList.replace('game', 'none' )
-        document.getElementById("science").classList.replace('science', 'none' )
-        document.getElementById("acquaintance").classList.replace('team', 'none' )
-        document.getElementById("calc").classList.replace('calc', 'none' )
-        document.getElementById("hero").classList.replace('hero', 'none' )
-        document.getElementById("time").classList.replace('time', 'none' )
-        document.getElementById("number").classList.replace('numbers', 'none' )
-
-      
+nuj2.addEventListener("click", () => {
+  document.getElementById("dino").classList.replace("game", "none");
+  document.getElementById("science").classList.replace("science", "none");
+  document.getElementById("acquaintance").classList.replace("team", "none");
+  document.getElementById("calc").classList.replace("calc", "none");
+  document.getElementById("hero").classList.replace("hero", "none");
+  document.getElementById("time").classList.replace("time", "none");
+  document.getElementById("number").classList.replace("numbers", "none");
 });
 
+const nul3 = document.getElementById("nul3");
 
-const nul3 = document.getElementById("nul3") 
-
-nul3.addEventListener('click', () => {
-        document.getElementById("dino").classList.replace('game', 'none' )
-        document.getElementById("calc").classList.replace('calc', 'none' )
-        document.getElementById("hero").classList.replace('hero', 'none' )
-        document.getElementById("time").classList.replace('time', 'none' )
-        document.getElementById("number").classList.replace('numbers', 'none' )
-        document.getElementById("game").classList.replace('game', 'none' )
-        document.getElementById("dino").classList.replace('dino', 'none' )
-        document.getElementById("socker").classList.replace('socker', 'none' )
-      
-       
-      
+nul3.addEventListener("click", () => {
+  document.getElementById("dino").classList.replace("game", "none");
+  document.getElementById("calc").classList.replace("calc", "none");
+  document.getElementById("hero").classList.replace("hero", "none");
+  document.getElementById("time").classList.replace("time", "none");
+  document.getElementById("number").classList.replace("numbers", "none");
+  document.getElementById("game").classList.replace("game", "none");
+  document.getElementById("dino").classList.replace("dino", "none");
+  document.getElementById("socker").classList.replace("socker", "none");
 });
-
-
-// game
-
-
-
-
-
-// 1 5 рік число калк калкчяс 3чис
-// 2 3 футб дин кнп 
-// 3 2 ком вчен 
-
-
-
-
-
-// nun1.addEventListener ("click", () => {
-//     game.stile.display = none
-// })
-
-
-
-
-
-
-
-
-
-
-
-
