d4470530f842afa9550db34a2d331215b498cc21
Shad3 <122724277+LONELY-DUckWolf@users.noreply.github.com>
2025-03-12 22:58:32 +0200

    фуфелшмермц минатор

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,17 +1,29 @@
 const openBtn = document.querySelector(".open__button");
 const closeBtn = document.querySelector(".modal__close");
 const backdrop = document.querySelector(".backdrop");
+const emailInput = document.querySelector(".footer__mail");
+const form = document.querySelector("#subscribeForm");
 
-openBtn.addEventListener("click", () => {
-  backdrop.classList.add("backdrop--visible");
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const emailValue = emailInput.value.trim();
+
+  if (emailValue.includes("@")) {
+    backdrop.classList.add("backdrop--visible");
+    document.body.classList.add("no-scroll");
+    form.reset(); 
+  }
 });
 
 closeBtn.addEventListener("click", () => {
   backdrop.classList.remove("backdrop--visible");
+  document.body.classList.remove("no-scroll");
 });
 
 backdrop.addEventListener("click", (event) => {
   if (event.target === backdrop) {
     backdrop.classList.remove("backdrop--visible");
+    document.body.classList.remove("no-scroll");
   }
 });
